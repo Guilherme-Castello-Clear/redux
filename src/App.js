e48c9738ef6d998e1724695a31f3d7b4dd5918dc
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-05 10:12:55 -0300

    Feat: Added Cards

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Card from './components/Card'
 function App() {
   return (
     <div className="App">
-      <Card/>
+      <h1>Exercicio React-Redux Simples</h1>
+      <div className='linha'>
+        <Card title="Card 1" red>X</Card>
+      </div>
+      <div className='linha'>
+        <Card title='Card 2' purple>Y</Card>
+        <Card title="Card 3" green>X</Card>
+        <Card title="Card 4" blue>X</Card>
+      </div>
     </div>
   );
 }
