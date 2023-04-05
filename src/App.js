4c7a068924e1f25ad93daf37e5eeda0100810ff9
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-05 11:10:03 -0300

    Feat: Added direct/indirect comunication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import react, { useState } from 'react'
 import Media from './components/Media'
 import Soma from './components/Soma';
 import Sorteio from './components/Sorteio';
 import Intervalo from './components/Intervalo';
 
 function App() {
+
+
+  const [min, setMin] = useState(10)
+  const [max, setMax] = useState(20)
+
+
   return (
     <div className="App">
       <h1>Exercicio React-Redux Simples</h1>
       <div className='linha'>
-        <Intervalo>X</Intervalo>
+        <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
       </div>
       <div className='linha'>
-        <Media></Media>
-        <Soma></Soma>
-        <Sorteio></Sorteio>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
       </div>
     </div>
   );
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -4,12 +4,16 @@ import React from 'react'
 import Card from './Card'
 
 export default props =>{
+
+    const {min, max} = props
+
+
     return(
         <Card title='Soma dos Números' blue>
             <div>
                 <span>
                     <span>Resultado</span>
-                    <strong>10</strong>
+                    <strong>{max+min}</strong>
                 </span>
             </div>
         </Card>
