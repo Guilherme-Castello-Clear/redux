b8bd80527e7dc602359593b475c68c3daa5a379b
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-06 08:36:22 -0300

    Feat: Redux refactored

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,39 +1,10 @@
 import { createStore, combineReducers }from 'redux'
-
+import numerosReducer from './reducers/numerosReducer'
 
 const reducers = combineReducers({
 
+    numeros: numerosReducer,
     
-    numeros: function(state, action){
-        switch(action.type){
-            case 'NUM_MIN_ALTERADO':
-                return{
-                    ...state,
-                    min: action.payload
-                }
-
-            case 'NUM_MAX_ALTERADO':
-                return{
-                    ...state,
-                    max: action.payload
-                }
-
-            default:
-                return{
-                    min: 7,
-                    max: 31
-                }
-        }
-    },
-    
-    nomes: function(state, action){
-
-        return [
-            'Ana',
-            'Bia',
-            'Carlos'
-        ]
-    }
 })
 
 function storeConfig(){
